022594b3005d8cab6c9165954177170e46a5705d
woeislolo <falloutgirl7@mail.ru>
2025-03-14 14:07:53 +0500

    feat: create models Category and Product and migrations

---
+++ online_store/store/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,
+                            verbose_name='Категория')
+    slug = models.SlugField(max_length=200,
+                            unique=True,
+                            verbose_name='Слаг')
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE,
+                                 verbose_name='Категория')
+    name = models.CharField(max_length=200,
+                            verbose_name='Наименование')
+    slug = models.SlugField(max_length=200,
+                            verbose_name='Слаг')
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True,
+                              verbose_name='Изображение')
+    description = models.TextField(blank=True,
+                                   verbose_name='Описание')
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                verbose_name='Цена')
+    available = models.BooleanField(default=True,
+                                    verbose_name='В наличии')
+    created = models.DateTimeField(auto_now_add=True,
+                                   verbose_name='Добавлен')
+    updated = models.DateTimeField(auto_now=True,
+                                   verbose_name='Изменен')
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
