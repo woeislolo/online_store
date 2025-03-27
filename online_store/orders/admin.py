ba42fbc8a78a5dda20e08f26b45ba539cc2bbdde
woeislolo <falloutgirl7@mail.ru>
2025-03-27 09:34:29 +0500

    feat: add method get_stripe_url() to model Order to receive link with payment info, add it to admin panel

--- online_store/orders/admin.py
+++ online_store/orders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
@@ -11,8 +12,16 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'post_code', 'city', 'paid',
-                    'created', 'updated']
+                    'address', 'post_code', 'city', 'paid', 
+                    'order_stripe_payment', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+
+    def order_stripe_payment(self, obj):
+        url = obj.get_stripe_url()
+        if obj.stripe_id:
+            return mark_safe(f'<a href="{url}" target="_blank" rel="noopener noreferrer">{obj.stripe_id}</a>')
+        return None
+
+    order_stripe_payment.short_description = 'id платежа (ссылка в Stripe)'
    
--- online_store/orders/models.py
+++ online_store/orders/models.py
@@ -39,6 +39,16 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+    def get_stripe_url(self):
+        """ Возвращает URL для платежа по заказу на сайте платежного шлюза Stripe """
+        if not self.stripe_id:
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            path = '/test/'
+        else:
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
 
 
 class OrderItem(models.Model):
