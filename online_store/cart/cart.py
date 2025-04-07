c3f337dd0f2bfab513f3688372fb9dc3c8a93f4f
woeislolo <falloutgirl7@mail.ru>
2025-04-07 12:07:48 +0500

    refactor: change methods order and docstring

--- online_store/cart/cart.py
+++ online_store/cart/cart.py
@@ -31,6 +31,16 @@ class Cart:
     def __len__(self):
         """ Считает кол-во товаров в корзине """
         return sum(item['quantity'] for item in self.cart.values())
+    
+    @property
+    def coupon(self):
+        """ Возвращает инстанс Coupon, если в корзине указан купон, либо None """
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
 
     def add(self, product, quantity=1, override_quantity=False):
         """ Добавляет товар в корзину или обновляет кол-во товара """
@@ -59,20 +69,10 @@ class Cart:
         """ Удаляет корзину из сессии """
         del self.session[settings.CART_SESSION_ID]
         self.save()
-
-    def get_total_price(self):
-        """ Получает общую сумму товаров в корзине """
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
     
-    @property
-    def coupon(self):
-        """ Возвращает инстанс Coupon, если в корзине указан купон, либо None """
-        if self.coupon_id:
-            try:
-                return Coupon.objects.get(id=self.coupon_id)
-            except Coupon.DoesNotExist:
-                pass
-        return None
+    def get_total_price(self):
+        """ Возвращает общую сумму товаров в корзине """
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def get_discount(self):
         """ Возвращает сумму скидки по купону """
